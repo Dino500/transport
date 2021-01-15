4722394c64aec32a5982fc8a3ab200fca2d2b8f2
Dino Topuz <dino.topuz@gmail.com>
2021-01-15 05:54:02 +0100

    Dodao ime u drower iz baze

--- screens/Drawcontent/Drowcontent.js
+++ screens/Drawcontent/Drowcontent.js
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from "react-native";
-import * as  firebase from "firebase/app";
-import React from "react";
+import * as firebase from "firebase/app";
+import React, { Component } from "react";
 import {
   Avatar,
   Title,
@@ -14,38 +14,59 @@ import {
 import { Ionicons } from "@expo/vector-icons";
 import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
 
-export function Drowercontent(props) {
-  return (
-    <View style={{ flex: 1 }}>
-      <DrawerContentScrollView {...props}>
-        <View>
-          <View style={{ flexDirection: "row", marginLeft: 15 }}>
-            <View>
-              <Avatar.Image
-                source={require("../../assets/icon.png")}
-                size={50}
-              />
-            </View>
-            <View style={{ marginLeft: 20 }}>
-              <Title>{firebase.firestore().doc("users").}</Title>
-              <Caption>{firebase.auth().currentUser.email}</Caption>
+var useres;
+export class Drowercontent extends Component {
+  state = {
+    name: "",
+  };
+
+  constructor(prop) {
+    super(prop);
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(firebase.auth().currentUser.uid)
+      .get()
+      .then((snapshot) => {
+        console.log(snapshot.data().name);
+        this.setState({
+          name: snapshot.data().name,
+        });
+      });
+  }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <DrawerContentScrollView {...this.props}>
+          <View>
+            <View style={{ flexDirection: "row", marginLeft: 15 }}>
+              <View>
+                <Avatar.Image
+                  source={require("../../assets/icon.png")}
+                  size={50}
+                />
+              </View>
+              <View style={{ marginLeft: 20 }}>
+                <Title>{this.state.name}</Title>
+                <Caption>{firebase.auth().currentUser.email}</Caption>
+              </View>
             </View>
           </View>
-        </View>
-      </DrawerContentScrollView>
-      <Drawer.Section style={styles.boromdrower}>
-        <Drawer.Item
-          label="Odjavi se"
-          icon={({ color, size }) => (
-            <Ionicons name="log-out" color={color} size={size} />
-          )}
-          onPress={() => {
-            firebase.auth().signOut();
-          }}
-        ></Drawer.Item>
-      </Drawer.Section>
-    </View>
-  );
+        </DrawerContentScrollView>
+        <Drawer.Section style={styles.boromdrower}>
+          <Drawer.Item
+            label="Odjavi se"
+            icon={({ color, size }) => (
+              <Ionicons name="log-out" color={color} size={size} />
+            )}
+            onPress={() => {
+              firebase.auth().signOut();
+            }}
+          ></Drawer.Item>
+        </Drawer.Section>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
